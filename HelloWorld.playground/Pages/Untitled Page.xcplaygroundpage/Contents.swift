0ac7cce12fc6e3f65afbdb4eb4de83b684981ba0
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-01-16 21:01:41 +0700

    push

---
+++ HelloWorld.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var name = "Teguh Prasetyo"
+
+print(name)
+
+//
+let a = 10
+let b = 20
+
+let result = a * b
+
+print(result)
+
+let WelcomeMessage:String
+
+WelcomeMessage = "Hello"
+
+print(WelcomeMessage)
